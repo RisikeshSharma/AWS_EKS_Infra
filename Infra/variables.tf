ff2ee98491fda722cd39384a8b4a5478ec8c5f14
Rishikesh Sharma <srishikesh39@gmail.com>
2026-08-12 11:06:51 +0530

    Update variables.tf

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -1,29 +1,43 @@
 variable "aws_region" {
   description = "AWS Region"
-  type        = string
-  default     = "us-east-1"
+
+  type = string
+
+  default = "us-east-1"
 }
+
 
 variable "cluster_name" {
   description = "EKS Cluster Name"
-  type        = string
-  default     = "k8s-lab-cluster"
+
+  type = string
+
+  default = "k8s-lab-cluster"
 }
+
 
 variable "cluster_version" {
-  description = "Kubernetes Version"
-  type        = string
-  default     = "m7i-flex.large"
+  description = "EKS Kubernetes Version"
+
+  type = string
+
+  default = "1.36"
 }
+
 
 variable "node_instance_type" {
-  description = "EC2 Worker Node Instance Type (Free Tier eligible)"
-  type        = string
-  default     = "t3.large"
+  description = "EC2 Worker Node Instance Type"
+
+  type = string
+
+  default = "m7i-flex.large"
 }
 
+
 variable "ami_type" {
-  description = "AMI Type for Node Group"
-  type        = string
-  default     = "AL2023_x86_64_STANDARD"
+  description = "EKS Worker Node AMI Type"
+
+  type = string
+
+  default = "AL2023_x86_64_STANDARD"
 }
